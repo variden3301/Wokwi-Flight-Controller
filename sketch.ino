b42be57942052013ad24efd84a727e59016ceb95
Bahadır Erdem <24529148+BeaSt2518@users.noreply.github.com>
2024-05-13 21:16:46 +0300

    MPU6050 Rotation Added

--- sketch.ino
+++ sketch.ino
@@ -1,5 +1,4 @@
-//
-
+//MPU Gyroscope Libraries
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
@@ -8,11 +7,46 @@
 #define PIN_TRIG 3
 #define PIN_ECHO 2
 
+//Fefine the mpu as mpu
+Adafruit_MPU6050 mpu;
+
+//MPU5060 Event
+sensors_event_t event;
+
 
 void setup() {
+  //Communication rate
   Serial.begin(115200);
+  //Ultrasonic setup
   pinMode(PIN_TRIG, OUTPUT);
   pinMode(PIN_ECHO, INPUT);
+
+  // Try to initialize the mpu sensor
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 initialized succesfully");
+  
+  
+  mpu.setGyroRange(MPU6050_RANGE_2000_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
 
 
 }
@@ -31,6 +65,17 @@ void loop() {
   // Start a new measurement:
   int distance = distancefinder();
   Serial.println(distance);
+  //Get sensor event for acceleration, rotation and temperature
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  Serial.print("Rotation X: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
 
   delay(1000);
 }
