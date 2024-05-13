33d576a7c5cf56f252915fcdccdf6a18dc74f03d
Bahadır Erdem <24529148+BeaSt2518@users.noreply.github.com>
2024-05-14 00:11:02 +0300

    Added stepper motor control for altitude control .
    Will ad 4 motors and motor parameter functionality later

--- sketch.ino
+++ sketch.ino
@@ -7,12 +7,19 @@
 #define PIN_TRIG 3
 #define PIN_ECHO 2
 
+
 //Fefine the mpu as mpu
 Adafruit_MPU6050 mpu;
+#define MPU_LED 25
 
 //MPU5060 Event
 sensors_event_t event;
 
+//First stepper motor test pins
+#define stepPin 31
+#define dirPin 33
+
+int currentStep = 0;
 
 void setup() {
   //Communication rate
@@ -20,6 +27,10 @@ void setup() {
   //Ultrasonic setup
   pinMode(PIN_TRIG, OUTPUT);
   pinMode(PIN_ECHO, INPUT);
+
+  //stepper test
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
 
   // Try to initialize the mpu sensor
   if (!mpu.begin()) {
@@ -29,6 +40,8 @@ void setup() {
     }
   }
   Serial.println("MPU6050 initialized succesfully");
+  pinMode(MPU_LED, OUTPUT);
+  digitalWrite(MPU_LED, HIGH);
   
   
   mpu.setGyroRange(MPU6050_RANGE_2000_DEG);
@@ -48,8 +61,21 @@ void setup() {
     break;
   }
 
+  //Stepper alignment
+  digitalWrite(dirPin,HIGH);
+  digitalWrite(stepPin,HIGH); 
+  delayMicroseconds(2000); 
+  digitalWrite(stepPin,LOW); 
+  delayMicroseconds(2000);
+  digitalWrite(stepPin,HIGH); 
+  delayMicroseconds(2000); 
+  digitalWrite(stepPin,LOW); 
+  delayMicroseconds(2000);
+
 
 }
+
+
 
 
 float distancefinder(){
@@ -59,6 +85,49 @@ float distancefinder(){
 
   int duration = pulseIn(PIN_ECHO, HIGH);
   return duration / 58;
+}
+
+int stepperTest(int distanceData){
+  
+  // Makes 200 pulses for making one full cycle rotation
+  
+  int stepCount = 0;
+  Serial.print("Distancedata: ");
+  Serial.println(distanceData);
+  Serial.print("CurrentStep: ");
+  Serial.println(currentStep);
+    if(currentStep < distanceData){
+      Serial.println("currentStep < distanceData");
+      digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
+      for(int x = 0; x < distanceData; x++) {
+        digitalWrite(stepPin,HIGH); 
+        delayMicroseconds(2000); 
+        digitalWrite(stepPin,LOW); 
+        delayMicroseconds(2000); 
+        stepCount++;
+      } 
+      currentStep = currentStep + stepCount;
+      stepCount = 0;
+  }
+  else if(currentStep > distanceData)
+  {
+    
+      digitalWrite(dirPin,LOW); //Changes the rotations direction
+      for(int x = 0;  x < currentStep - distanceData; x++) {
+      digitalWrite(stepPin,HIGH);
+      delayMicroseconds(2000);
+      digitalWrite(stepPin,LOW);
+      delayMicroseconds(2000);
+      stepCount++;
+      } 
+      currentStep = currentStep - stepCount;
+      stepCount = 0;
+
+  }
+
+    
+    delay(1000); // One second delay
+  return currentStep;
 }
 
 void loop() {
@@ -76,6 +145,8 @@ void loop() {
   Serial.print(", Z: ");
   Serial.print(g.gyro.z);
   Serial.println(" rad/s");
+  stepperTest(distance);
+  
 
   delay(1000);
 }
